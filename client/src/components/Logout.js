6d6146702155b821da4af000ef520a664a27bc07
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-14 13:13:34 -0500

    lecture work-in-progress

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const Logout = (props)=> {
-    const { push } = props.history;
+    // const { push } = props.history;
+    const { push } = useHistory();
 
     useEffect(()=> {
         axios.post('http://localhost:5001/api/logout', {}, {
