db05e084dc77676c3efa612e050ca9e43ede3897
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-14 13:46:50 -0500

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = (props) => {
+const PrivateRoute = ({component, ...rest  }) => {
     return <Route {...props}/>
 }
 
