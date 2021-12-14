d5101ca02b224b88732ec4c926694b0e87aa4655
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-14 12:37:02 -0500

    lecture work-in-progress

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -20,6 +20,9 @@ class Login extends React.Component {
 
   login = e => {
     e.preventDefault();
+    //1. make an axios call to login (add our user information)
+    //2. if request is successful, save our token to localStorage
+    //3. if request is not successful, log error messsage.
   };
 
   render() {
