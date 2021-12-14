c7a04eaa0833356c14e3d195a55436bfb1b74e68
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-14 13:27:24 -0500

    lecture work-in-progress

--- client/src/components/GasPrices.js
+++ client/src/components/GasPrices.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import Loader from 'react-loader-spinner';
-import axios from 'axios';
+import axiosWithAuth from './../utils/axiosWithAuth';
 
 class GasPrices extends React.Component {
   state = {
