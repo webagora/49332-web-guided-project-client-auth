72dfa9c0a4166c49dcebfa584b57f64c65cfa76e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-14 13:50:24 -0500

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -3,10 +3,11 @@ import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({component:Component, ...rest  }) => {
     return <Route {...rest} render={()=> {
-        return(<div>
-            <Component/>
-            <p>aefasfd</p>
-        </div>);
+        if (localStorage.getItem("token")) {
+            return(<Component/>)
+        } else {
+            <Redirect to="/login"/>
+        }
     }}/>
 }
 
