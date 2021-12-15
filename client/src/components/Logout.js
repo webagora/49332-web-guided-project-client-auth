7727c9119627404bc13a46c35dfa6be58c8c51cb
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-14 17:12:37 -0800

    Logout.js - in place

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+import axios from 'axios'
+import { checkPropTypes } from "prop-types";
 
-const Logout = ()=> {
+const Logout = (props)=> {
+    
+    // const { push } = props.history;
+    const { push } = useHistory();
+   
+    useEffect (() =>{
+        axios.post ('http://localhost:5001/api/logout', {}, {
+            headers: {
+                authorization: localStorage.getItem("token")
+            }
+        })
+        .then (resp => {
+            localStorage.removeItem('token');
+            push("/login");            
+        })
+        .catch (err => {
+            console.log(err);
+        })
+    }) 
+
     return(<div></div>);
 }
 
