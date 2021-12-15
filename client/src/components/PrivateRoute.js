14ea80766dffe4e89d92ff158aef3a09529659d2
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-14 18:36:42 -0800

    PrivateRoute.js - Build PrivateRoute ...

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-const PrivateRoute = (props) => {
-    console.log('props in PrivateRoute: ', props);
-    return <Route {...props} />
+const PrivateRoute = ({ component, ...rest }) => {    
+    console.log('component: ', component);
+    console.log('rest: ', rest);
+    return <Route {...rest} render = {() => { 
+        return (<p>This is a protected Route</p>)
+    }} />
 }
 
 export default PrivateRoute;
