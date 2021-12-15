c7582bc6fc2adaa5cf7877242b046bad7fc3a746
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-14 18:47:03 -0800

    PrivateRoute.js - PrivateRoute in place

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -4,11 +4,11 @@ import { Route, Redirect } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => {    
     
     return <Route {...rest} render = {() => { 
-        return (
-            <div>
-                <Component />
-            </div>
-            )
+        if (localStorage.getItem("token")) {
+            return(<Component/>)
+        } else {
+            return <Redirect to="/login"/>
+        }
     }} />
 }
 
